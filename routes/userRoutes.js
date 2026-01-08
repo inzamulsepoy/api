bc25ee37d5e73e41050e7b52e82b3b1900ad410a
inzamulsepoy <inzamulsepoy@gmail.com>
2026-01-08 16:05:45 +0530

    changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUsers } = require('../controllers/userController');
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+} = require('../controllers/userController');
 
+// Create a new user
 router.post('/submit', createUser);
+
+// Get all users
 router.get('/all', getUsers);
 
+// Get single user by ID
+router.get('/:id', getUserById);
+
+// Update user by ID
+router.put('/:id', updateUser);
+
+// Delete user by ID
+router.delete('/:id', deleteUser);
+
 module.exports = router;
+

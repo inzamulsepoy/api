5c87640eb73438eb2e6df42ad792525f51e90bb1
inzamulsepoy <inzamulsepoy@gmail.com>
2026-01-11 13:55:28 +0530

    api changes

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,17 @@
-const db = require('../config/db');
-
-const User = {
-    create: async (name, email) => {
-        const [result] = await db.execute(
-            'INSERT INTO users (name, email) VALUES (?, ?)',
-            [name, email]
-        );
-        return result;
-    },
-    getAll: async () => {
-        const [rows] = await db.execute('SELECT * FROM users ORDER BY id DESC');
-        return rows;
-    }
-};
-
-module.exports = User;
+const db = require('../config/db'); 
+ 
+const User = { 
+    create: async (name, email) => { 
+        const [result] = await db.execute( 
+            'INSERT INTO users (name, email) VALUES (?, ?)', 
+            [name, email] 
+        ); 
+        return result; 
+    }, 
+    getAll: async () => { 
+        const [rows] = await db.execute('SELECT * FROM users ORDER BY id ASC'); 
+        return rows; 
+    } 
+}; 
+ 
+module.exports = User; 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,14 +14,14 @@ router.post('/submit', createUser);
 // Get all users
 router.get('/all', getUsers);
 
-// Get single user by ID
-router.get('/:id', getUserById);
+// // Get single user by ID
+// router.get('/:id', getUserById);
 
-// Update user by ID
-router.put('/:id', updateUser);
+// // Update user by ID
+// router.put('/:id', updateUser);
 
-// Delete user by ID
-router.delete('/:id', deleteUser);
+// // Delete user by ID
+// router.delete('/:id', deleteUser);
 
 module.exports = router;
 
